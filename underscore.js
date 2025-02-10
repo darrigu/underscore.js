8c6890cdfa4223902502e2af61809b643935218d
LeBakaSable <hugogole.ito@gmail.com>
2025-02-10 07:23:44 +0100

    feat: update

--- underscore.js
+++ underscore.js
@@ -1,64 +1,82 @@
+const appendChildren = (node, children) => {
+   for (const child of children) {
+      if (typeof child === 'string') {
+         node.append(document.createTextNode(child))
+      } else if (typeof child === 'function') {
+         const maybeChild = child(node);
+         if (maybeChild) {
+            node.append(maybeChild);
+         }
+      } else if (child) {
+         node.append(child);
+      }
+   }
+};
+
+const toKebab = (name) => name.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+
 function _(name, ...children) {
-  const result = document.createElement(name)
+   const result = document.createElement(name);
+   appendChildren(result, children);
 
-  for (const child of children) {
-    if (typeof (child) === 'string')
-      result.appendChild(document.createTextNode(child))
-    else
-      result.appendChild(child)
-  }
+   result._att = function (atts) {
+      for (const [name, value] of Object.entries(atts))
+         this.setAttribute(toKebab(name), value)
+      return this;
+   };
 
-  result._att = function(atts) {
-    for (const [name, value] of Object.entries(atts))
-      this.setAttribute(name, value)
-    return this
-  }
+   result._onclick = function (callback) {
+      this.onclick = callback;
+      return this;
+   };
 
-  result._onclick = function(callback) {
-    this.onclick = callback
-    return this
-  }
+   result._onsubmit = function (callback) {
+      this.onsubmit = callback;
+      return this;
+   };
 
-  return result
+   return result;
 }
 
-const MUNDANE_TAGS = ['canvas', 'h1', 'h2', 'h3', 'p', 'a', 'div', 'span', 'nav', 'ul', 'li', 'strong']
-for (let tagName of MUNDANE_TAGS)
-  window['_' + tagName] = (...children) => _(tagName, ...children)
+for (let tagName of ['abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'meta', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr'])
+   window['_' + tagName] = (...children) => _(tagName, ...children);
 
-function _img(src) {
-  return _('src')._att({ src })
-}
+const _a = (name, href = '') => _('a', name)._att({ href });
+const _img = (src) => _('img')._att({ src })
+const _input = (type) => _('input')._att({ type });
 
-function _input(type) {
-  return _('input')._att({ type });
-}
+// TODO: nested routers
+// TODO: ability to animate page change
+const _router = (routes, node = _div) => {
+   let result = node();
 
-function _router(routes) {
-  let _result = _div()
+   const syncHash = () => {
+      let hashLocation = document.location.hash.split('#')[1] || '/';
 
-  function syncHash() {
-    let hashLocation = document.location.hash.split('#')[1]
-    if (!hashLocation)
-      hashLocation = '/'
+      if (!(hashLocation in routes)) {
+         const route404 = '/404';
+         console.assert(route404 in routes);
+         hashLocation = route404;
+      }
 
-    if (!(hashLocation in routes)) {
-      const route404 = '/404'
-      console.assert(route404 in routes)
-      hashLocation = route404
-    }
+      const child = routes[hashLocation](result);
+      if (Array.isArray(child)) {
+         result.innerHTML = '';
+         appendChildren(result, child);
+      } else {
+         result.replaceChildren(child);
+      }
 
-    while (_result.firstChild)
-      _result.removeChild(_result.lastChild)
+      return result;
+   };
 
-    _result.appendChild(routes[hashLocation])
+   syncHash();
 
-    return _result;
-  }
+   window.addEventListener("hashchange", syncHash);
 
-  syncHash()
+   result.refresh = syncHash;
 
-  window.addEventListener("hashchange", syncHash);
+   return result;
+};
 
-  return _result
-}
+// TODO: signal system
